ca6bb08a898d5877560e9ee7b02f461f13178f33
kdokic1 <kdokic1@etf.unsa.ba>
2021-10-07 18:27:29 +0200

    fronted/client side set

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './Navbar';
+
+function App() {
+  const title = 'Welcome to the new blog';
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <h1>{ title }</h1>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
