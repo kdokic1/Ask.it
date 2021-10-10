d2678ebf7c8216e09fec8a79a672260e34413606
kdokic1 <kdokic1@etf.unsa.ba>
2021-10-10 23:17:42 +0200

    tables added

--- config/database.js
+++ config/database.js
@@ -14,10 +14,8 @@ const sequelize = new Sequelize(process.env.NODE_ENV === "production" ?  proConf
     define: {
     timestamps: false
     },
-    dialectOptions: {
-        ssl: {
-            rejectUnauthorized: false
-        }
+    ssl: {
+        rejectUnauthorized: false
     }
 });
 
